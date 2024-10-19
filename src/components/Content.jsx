a15b158bcdeb591e52ca01ea9ab51b05d9d6629d
Kaushal <kaushalkr8@gmail.com>
2024-10-19 12:08:32 +0530

    changes in filter

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import useFetch from "../useFetch";
 import { Link } from 'react-router-dom'
 
 
 
 const Content = () => {
-    // const { data } = useFetch('http://localhost:3000/allEvent')
+    const[filter,setFilter]=useState("")
     const { data } = useFetch('https://bi-assigment-backend.vercel.app/allEvent')
+
+
+const filteredData = filter
+    ? data.filter((item) => item.eventType === filter) // Apply filter if selected
+    : data;
    
     return (
         <>
@@ -15,7 +21,8 @@ const Content = () => {
                         <h1 className="fw-bold">Meetup Event</h1>
                     </div>
                     <div className="col-md-6">
-                        <select className="form-select">
+                        <select className="form-select" value={filter} onChange={e=>setFilter(e.target.value)}>
+                            <option value="" deselect>All</option>
                             <option value="Offline">Offline</option>
                             <option value="Online">Online</option>
                         </select>
@@ -24,7 +31,7 @@ const Content = () => {
 
                 <div className="m-4">
                     <div className="row m-4">
-                        {data?.map((event) => (
+                        {filteredData?.map((event) => (
                             <div className="col-md-4" key={event._id}>
                                 <div className="card mb-3 border-0 bg-secondary-subtle" style={{ width: "18rem" }} >
                                     <img src={event.eventImage} className="card-img-top rounded" alt={event.eventName} />
